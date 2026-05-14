dfd3bff275276232a3176c6e4356c082916fb80d
Tanvi2307 <tanviagarwal785@gmail.com>
2026-05-15 00:13:12 +0530

    Updated logger.py

--- src/logger.py
+++ src/logger.py
@@ -9,7 +9,7 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE) #Builds the full path to the a
 
 logging.basicConfig(
     filename = LOG_FILE_PATH,
-    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
 )
 
