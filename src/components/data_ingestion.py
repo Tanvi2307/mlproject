4fd68781c611984fd5141a526d665c49390cdd32
Tanvi2307 <tanviagarwal785@gmail.com>
2026-05-16 23:51:52 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,15 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #used to create data variables
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
-class DataIngestionConfig:
+class DataIngestionConfig: #it is like all the input things that are required
     train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
     raw_data_path: str = os.path.join('artifacts', "data.csv")
+#Think of DataIngestionConfig as a settings object — one place where all file paths live. If you ever want to change where files are saved, you change it here and it updates everywhere.
     
 class DataIngestion:
     def __init__(self):
@@ -35,8 +38,11 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e, sys) #If anything goes wrong anywhere in the try block — catches it and raises our CustomException with full file name + line number detail.
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
